dd222e6141d15671ed94d146278533d10a280b4d
matheuspython <ruivorobado@gmail.com>
2022-04-19 20:50:32 -0300

    aprendendo state com classes

--- src/App.js
+++ src/App.js
@@ -4,24 +4,32 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component{
+  constructor (props){
+    super(props)
+    this.state = {
+      name: 'matheus souza'
+    }
+  }
+  
   render(){
+    const name = this.state.name
     return (
       <div className="App">
-             <header className="App-header">
-               <img src={logo} className="App-logo" alt="logo" />
-               <p>
-                 Edit <code>src/App.js</code> and save to reload.
-               </p>
-               <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              > 
-                Learn React
-              </a>
-            </header>
-          </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+           {name}
+          </p>
+          <a
+           className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer"
+          > 
+           Learn React
+          </a>
+        </header>
+     </div>
     )
   }
 }
